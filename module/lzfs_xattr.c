c6f658a07ac540c8e9c4fdf0acbc27d5911f8567
Neependra Khare <neependra@kqinfotech.com>
2010-12-29 16:47:20 +0530

    For getattr return ENODATA only in lookup returns ENOENT else send back err.

--- module/lzfs_xattr.c
+++ module/lzfs_xattr.c
@@ -35,9 +35,12 @@ lzfs_xattr_get(struct inode *inode, const char *name,
 	dvp = LZFS_ITOV(inode);
 	err = zfs_lookup(dvp, NULL, &vp, NULL, LOOKUP_XATTR, NULL,
 			(struct cred *) cred, NULL, NULL, NULL);
-	if(err) {
-		return -ENODATA;
-	}
+        if(err) {
+            if(err == ENOENT) {
+                return -ENODATA;
+            }
+            return -err;
+        }
 	ASSERT(vp != NULL);
 	if(index == 0) {
 		xattr_name = kzalloc(strlen(name) + 6, GFP_KERNEL);
